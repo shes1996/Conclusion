c1066651a65a025ee1a79ba09dd1b92ff14206d7
shes1996 <shes1996@bk.ru>
2022-09-10 14:04:04 +0300

    filter for empty values

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿string[] words = {"hel", "2", "world",":-)"};
+﻿string[] words = {"hello", "2", "world",":-)"};
 int count = 0;
 for (int i = 0; i < words.Length; ++i)
     {
@@ -11,5 +11,5 @@ for (int i = 0; i < words.Length; ++i)
     }
 for (int j = 0; j < words.Length; j++)
      {
-         Console.Write(itog[j] + ' ');
+        if (itog[j] != null) Console.Write(itog[j] + ' ');
      }
