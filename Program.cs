2bd5c3904068bfda0c1c988f4c6993574c6287ff
shes1996 <shes1996@bk.ru>
2022-09-10 14:41:22 +0300

    final

--- Program.cs
+++ Program.cs
@@ -1,15 +1,14 @@
 ﻿string[] words = {"hello", "2", "world",":-)"};
-int count = 0;
-for (int i = 0; i < words.Length; ++i)
-    {
-        if (words[i].Length <= 3) count++;
-    }
 string[] itog = new string[words.Length];
 for (int i = 0; i < words.Length; ++i)
     {               
         if (words[i].Length <= 3) itog[i] = words[i];
     }
 for (int j = 0; j < words.Length; j++)
-     {
+    {
         if (itog[j] != null) Console.Write(itog[j] + ' ');
-     }
+    }
+
+
+
+
